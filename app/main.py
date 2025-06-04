ae6a8d8c1c74ee18c894ffa2f70c6766c12139a9
Jospin <jospinndagano1@gmail.com>
2025-06-04 14:08:27 +0200

    add alembic

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+# from app.routes.item_route import router as item_router
+from contextlib import asynccontextmanager
+from app.config.db import init_db
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    init_db()
+    yield
+
+app = FastAPI(title="FastAPI with PostgreSQL", lifespan=lifespan, version="0.1")
+
+
+# app.include_router(item_router, prefix="/api/v1/items", tags=["items"])
 
 @app.get("/")
-def read_root():
-    return {"message": "Bienvenue sur FastAPI"}
+async def root():
+    return {"message": "Hello World"}
